bed29e2d14144f736abc2c952bb0133b95387cbb
Przemek <pzawodzinski@gmail.com>
2023-01-15 12:44:39 +0100

    User update

--- src/main/java/com/sda/mode1/User.java
+++ src/main/java/com/sda/mode1/User.java
@@ -2,13 +2,18 @@ package com.sda.mode1;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 @Entity
 public class User {
 
@@ -20,5 +25,17 @@ public class User {
     private int age;
     private String email;
 
+ @Override
+ public boolean equals(Object o) {
+  if (this == o) return true;
+  if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
 
+  User user = (User) o;
+  return username != null && Objects.equals(username, user.username);
+ }
+
+ @Override
+ public int hashCode() {
+  return getClass().hashCode();
+ }
 }
